9b0e36b7fdedd3cf66832d7f93aa40e2686a4214
Chin Isradisaikul <chinawat@cs.cornell.edu>
2019-01-20 20:49:04 +0700

    Added release code and test cases for hw2.

---
+++ hw/src/cpecmu/cpe218/sp2019/hw2/GraphBuilder.java
@@ -0,0 +1,21 @@
+package cpecmu.cpe218.sp2019.hw2;
+
+import cpecmu.cpe218.sp2019.Graph;
+
+public interface GraphBuilder {
+
+    /**
+     * Construct, if possible, an undirected graph in which each vertex
+     * has the given degree.
+     * @param <V> the vertex type
+     * @param degrees the degree of each vertex
+     * @param vertices the list of vertices, whose length is the same as
+     *          the length of {@code degrees}
+     * @return an undirected graph whose vertices are {@code vertices}, where
+     *          {@code vertices[i]} has degree {@code degrees[i]};
+     *          or {@code null} if it is impossible to construct such a graph
+     *          from the given input
+     */
+    <V> Graph<V> buildGraph(int[] degrees, V[] vertices);
+
+}
---
+++ hw/src/cpecmu/cpe218/sp2019/hw2/Subseq.java
@@ -0,0 +1,18 @@
+package cpecmu.cpe218.sp2019.hw2;
+
+import java.util.List;
+
+public interface Subseq {
+
+    /**
+     * Determine whether the given patterns is a subsequence in the haystack.
+     * @param <E> the element types
+     * @param patterns a sequence of items to look for
+     * @param haystack a sequence of items to be searched
+     * @return {@code true} if there is a way to delete some items in
+     *          {@code haystack} so that the resulting sequence is equal to
+     *          {@code patterns}; {@code false} otherwise
+     */
+    <E> boolean isSubsequence(List<E> patterns, List<E> haystack);
+
+}
